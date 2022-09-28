661e4bbbce8604abb981dec2fd47e38829954f5a
Oliver Giles <ogiles@email.com>
2022-09-28 19:57:14 +0100

    fixed add mod

--- src/lib.rs
+++ src/lib.rs
@@ -18,5 +18,6 @@ fn sum_as_string_add_one(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn bscholes(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(sum_as_string_add_one, m)?)?;
     Ok(())
 }
